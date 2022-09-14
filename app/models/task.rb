1e47454ea001e42d45757f44350aab384c7f72b6
Swaraj Deshmukh <swaraj.deshmukh@aventior.com>
2022-09-14 21:23:42 +0530

    Added tasks controller and index action

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,2 @@
 class Task < ApplicationRecord
-end    
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_13_080855) do
+ActiveRecord::Schema.define(version: 2022_09_14_121058) do
 
   create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
